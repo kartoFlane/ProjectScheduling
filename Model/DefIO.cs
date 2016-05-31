kartoFlane/ProjectScheduling

commit 3ad4ee579e9d1c60cc3948a241916e0c7f7d661d

Removed optimizations which caused the GA to actually slow down. Updated WriteDEF() to match evaluation function.

Model/DefIO.cs
@@ -97,70 +97,90 @@ namespace ProjectScheduling.Model
 			buf.Append( " Resource assignments (resource ID - task ID) " );
 			buf.Append( "\n" );
 
-			// Preallocate space in the collections. Saves ~6% of runtime.
-			Dictionary<int, int> busyResourceMap = new Dictionary<int, int>();
-			Dictionary<int, int> resourceTaskMap = new Dictionary<int, int>();
+			int[] busyResources = new int[env.Resources.Length];
+			int[] resourceTasks = new int[env.Resources.Length];
+			bool[] completedTasks = new bool[env.Tasks.Length];
 
 			foreach ( Resource r in env.Resources ) {
-				busyResourceMap[r.Id] = -1;
-				resourceTaskMap[r.Id] = -1;
+				busyResources[r.Id] = -1;
+				resourceTasks[r.Id] = -1;
 			}
 
-			HashSet<int> completedTasks = new HashSet<int>();
-
-			List<string> prereqs = new List<string>();
-
-			int offset = env.Tasks.Length;
-			List<TaskData> pendingTasks = new List<TaskData>( offset );
-			for ( int i = 0; i < offset; ++i ) {
-				pendingTasks.Add( new TaskData( i, specimen.Genotype[i], specimen.Genotype[i + offset] ) );
-			}
-			pendingTasks = pendingTasks.OrderBy( td => td.Priority ).ToList();
-
+			int completedTasksCount = 0;
 			int currentTime = 0;
 			double totalCost = 0;
 
-			while ( completedTasks.Count < env.Tasks.Length ) {
+			int offset = env.Tasks.Length;
+			TaskData[] pendingTasks = new TaskData[offset];
+			for ( int i = 0; i < offset; ++i ) {
+				TaskData td = new TaskData( i, specimen.Genotype[i], specimen.Genotype[i + offset] );
+				td.Task = env.Tasks[td.taskId];
+				td.Resource = env.Resources[td.ResourceId];
+				pendingTasks[i] = td;
+			}
+
+			pendingTasks = pendingTasks.OrderBy( td => td.Priority ).ToArray();
+
+			while ( completedTasksCount < offset ) {
 				bool newRow = false;
 				++currentTime;
 
 				// Check whether any tasks have been completed at this time step.
+				bool allBusy = true;
+
+				int earliest = int.MaxValue;
 				foreach ( Resource r in env.Resources ) {
-					int taskDoneTime = busyResourceMap[r.Id];
+					int taskDoneTime = busyResources[r.Id];
+
+					if ( taskDoneTime > 0 && taskDoneTime < earliest ) {
+						earliest = taskDoneTime;
+					}
 
 					if ( taskDoneTime < 0 ) {
+						// A resource is idle
+						allBusy = false;
 					}
 					else if ( taskDoneTime <= currentTime ) {
-						Task t = env.Tasks[resourceTaskMap[r.Id]];
+						Task t = env.Tasks[resourceTasks[r.Id]];
 						totalCost += t.Duration * r.Cost;
 
-						completedTasks.Add( resourceTaskMap[r.Id] );
-						busyResourceMap[r.Id] = -1;
-						resourceTaskMap[r.Id] = -1;
+						completedTasks[t.Id] = true;
+						completedTasksCount++;
+						busyResources[r.Id] = -1;
+						resourceTasks[r.Id] = -1;
+
+						// A resource is being released
+						allBusy = false;
 					}
 				}
 
-				for ( int i = 0; i < pendingTasks.Count; ++i ) {
+				if ( allBusy ) {
+					// If all resources are currently busy, then don't bother needlessly
+					// looking for a task to insert.
+					// Instead, skip to the time when a resource is freed.
+					currentTime = earliest;
+					continue;
+				}
+
+				for ( int i = 0; i < offset; ++i ) {
 					TaskData td = pendingTasks[i];
+
+					if ( td == null )
+						continue;
 
 					Resource r = env.Resources[td.ResourceId];
 					Task t = env.Tasks[td.taskId];
 
-					if ( !t.Predecessors.All( p => completedTasks.Contains( p ) ) ) {
-						continue;
-					}
-
-					if ( busyResourceMap[td.ResourceId] >= 0 ) {
+					if ( busyResources[td.ResourceId] >= 0 ) {
 						// Resource is busy, we can't complete this task at this point in time.
 						continue;
 					}
 
 					// We can use the resource, so mark it as busy
-					busyResourceMap[td.ResourceId] = currentTime + t.Duration;
-					resourceTaskMap[td.ResourceId] = td.taskId;
+					busyResources[td.ResourceId] = currentTime + t.Duration;
+					resourceTasks[td.ResourceId] = td.taskId;
 
-					pendingTasks.Remove( td );
-					--i;
+					pendingTasks[i] = null;
 
 					if ( !newRow ) {
 						newRow = true;

Model/EnvironmentContext.cs
@@ -122,30 +122,23 @@ namespace ProjectScheduling.Model
 			int completedTasksCount = 0;
 			int currentTime = 0;
 			double totalCost = 0;
-			double penalty = 0;
 
 			int offset = Tasks.Length;
-			List<TaskData> pendingTasks = new List<TaskData>( offset );
+			TaskData[] pendingTasks = new TaskData[offset];
 			for ( int i = 0; i < offset; ++i ) {
 				TaskData td = new TaskData( i, specimen.Genotype[i], specimen.Genotype[i + offset] );
 				td.Task = Tasks[td.taskId];
 				td.Resource = Resources[td.ResourceId];
-				pendingTasks.Add( td );
+				pendingTasks[i] = td;
 			}
 
-			pendingTasks = pendingTasks.OrderBy( td => td.Priority ).ToList();
-
-			// TODO: Optionally sort by time to complete, cost, or pick randomly here,
-			// for tasks with the same priority.
-
-			TaskData[] validTasks = pendingTasks.Where( td => td.Task.Predecessors.Length == 0 ).ToArray();
+			pendingTasks = pendingTasks.OrderBy( td => td.Priority ).ToArray();
 
 			while ( completedTasksCount < offset ) {
 				++currentTime;
 
 				// Check whether any tasks have been completed at this time step.
 				bool allBusy = true;
-				bool wasReleased = false;
 
 				int earliest = int.MaxValue;
 				foreach ( Resource r in Resources ) {
@@ -170,7 +163,6 @@ namespace ProjectScheduling.Model
 
 						// A resource is being released
 						allBusy = false;
-						wasReleased = true;
 					}
 				}
 
@@ -181,22 +173,15 @@ namespace ProjectScheduling.Model
 					currentTime = earliest;
 					continue;
 				}
-				else if ( wasReleased ) {
-					// A resource was released, meaning a task was completed -- recompute valid tasks
-					validTasks = pendingTasks.
-						Where( td => td.Task.Predecessors.All( p => completedTasks[p] ) ).
-						OrderBy( td => td.Priority ).
-						ToArray();
-				}
 
-				for ( int i = 0; i < validTasks.Length; ++i ) {
-					TaskData td = validTasks[i];
+				for ( int i = 0; i < offset; ++i ) {
+					TaskData td = pendingTasks[i];
 
 					if ( td == null )
 						continue;
 
-					Resource r = td.Resource;
-					Task t = td.Task;
+					Resource r = Resources[td.ResourceId];
+					Task t = Tasks[td.taskId];
 
 					if ( busyResources[td.ResourceId] >= 0 ) {
 						// Resource is busy, we can't complete this task at this point in time.
@@ -207,12 +192,11 @@ namespace ProjectScheduling.Model
 					busyResources[td.ResourceId] = currentTime + t.Duration;
 					resourceTasks[td.ResourceId] = td.taskId;
 
-					validTasks[i] = null;
-					pendingTasks.Remove( td );
+					pendingTasks[i] = null;
 				}
 			}
 
-			return currentTime + penalty + totalCost / 100000;
+			return currentTime + totalCost / 100000;
 		}
 	}
 }

GeneticAlgorithm.cs
@@ -19,6 +19,7 @@ namespace ProjectScheduling
 		private DirectoryInfo outputDir;
 
 		private ProjectSchedule allTimeBest;
+		private long timeStart;
 
 		private Dictionary<int, double> minMap;
 		private Dictionary<int, double> avgMap;
@@ -148,7 +149,7 @@ namespace ProjectScheduling
 			Console.WriteLine();
 
 			int generationIndex = 0;
-			int timeStart = Environment.TickCount;
+			timeStart = Environment.TickCount;
 
 			while ( !RequestTerminate && generationIndex < GenerationLimit ) {
 				int genTimeStart = Environment.TickCount;
@@ -429,15 +430,19 @@ namespace ProjectScheduling
 		{
 			StringBuilder buf = new StringBuilder();
 
+			double totalTime = ( Environment.TickCount - timeStart ) / 1000.0;
+
 			buf.AppendFormat( "File: {0}\n", defFileName );
 			buf.AppendFormat( "Population: {0}\n", PopulationSize );
 			buf.AppendFormat( "Generations: {0}\n", GenerationLimit );
+			buf.AppendFormat( CultureInfo.InvariantCulture, "Total time: {0}s\n", totalTime );
 			buf.AppendFormat( CultureInfo.InvariantCulture, "Breeder fraction: {0}\n", BreederFraction );
 			buf.AppendFormat( CultureInfo.InvariantCulture, "Clone threshold: {0}\n", CloneThreshold );
 			buf.AppendFormat( CultureInfo.InvariantCulture, "Mutation chance: {0}\n", MutationChance );
 			buf.AppendFormat( CultureInfo.InvariantCulture, "Crossover chance: {0}\n", CrossoverChance );
 			buf.AppendFormat( "Clone elim. strategy: {0}\n", CloneElimination.ToString() );
 			buf.AppendFormat( "Crossover strategy: {0}\n", CrossoverStrategy.ToString() );
+			buf.AppendFormat( "Selection strategy: {0}\n", SelectionStrategy.ToString() );
 
 			File.WriteAllText( path, buf.ToString() );
 		}

commit fa9a977e1817f0c9752e55bcae146d5cf1223340

Fixed incomplete revert made in 3ad4ee5, which caused relations to be completed incorrectly.

Model/DefIO.cs
@@ -176,6 +176,10 @@ namespace ProjectScheduling.Model
 						continue;
 					}
 
+					if ( !t.Predecessors.All( p => completedTasks[p] ) ) {
+						continue;
+					}
+
 					// We can use the resource, so mark it as busy
 					busyResources[td.ResourceId] = currentTime + t.Duration;
 					resourceTasks[td.ResourceId] = td.taskId;

Model/EnvironmentContext.cs
@@ -186,6 +186,10 @@ namespace ProjectScheduling.Model
 						continue;
 					}
 
+					if ( !t.Predecessors.All( p => completedTasks[p] ) ) {
+						continue;
+					}
+
 					// We can use the resource, so mark it as busy
 					busyResources[td.ResourceId] = currentTime + t.Duration;
 					resourceTasks[td.ResourceId] = td.taskId;